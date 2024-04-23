529cfa956df3d019b48f692b4f649d6e4ec2b8b2
Johanna Eklundh <jojo.eklundh@gmail.com>
2024-04-23 09:57:38 +0200

    All players should be shown

--- Assets/Scripts/Menu2.cs
+++ Assets/Scripts/Menu2.cs
@@ -12,34 +12,34 @@ public class Menu2 : MonoBehaviour
 
     void Start()
     {
-        ShowObjects(); // Ensure object is hidden at the start
+        //ShowObjects(); // Ensure object is hidden at the start
     }
 
     void Update()
     {
         // Call the function to show objects based on the updated value of ConnectedPlayers
-        ShowObjects();
+        //ShowObjects();
     }
 
     // Function to show objects based on the value of ConnectedPlayers
-    void ShowObjects()
-    {
-        // Loop through all the objects in the objectsToShow array
-        for (int i = 0; i < objectsToShow.Length; i++)
-        {
-            // Check if the index is less than the value of ConnectedPlayers
-            // If so, activate the game object
-            if (i < ConnectedPlayers)
-            {
-                objectsToShow[i].SetActive(true);
-            }
-            // If not, deactivate the game object
-            else
-            {
-                objectsToShow[i].SetActive(false);
-            }
-        }
-    }
+    //void ShowObjects()
+    //{
+    //    // Loop through all the objects in the objectsToShow array
+    //    for (int i = 0; i < objectsToShow.Length; i++)
+    //    {
+    //        // Check if the index is less than the value of ConnectedPlayers
+    //        // If so, activate the game object
+    //        if (i < ConnectedPlayers)
+    //        {
+    //            objectsToShow[i].SetActive(true);
+    //        }
+    //        // If not, deactivate the game object
+    //        else
+    //        {
+    //            objectsToShow[i].SetActive(false);
+    //        }
+    //    }
+    //}
 
     public void PlayGame()
     {
