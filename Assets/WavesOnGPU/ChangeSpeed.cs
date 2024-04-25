d9f27524e0fac715704bad9c300b5e8e7d33a53d
Caity <caiwu993@student.liu.se>
2024-04-25 15:33:43 +0200

    fixxxx

--- Assets/WavesOnGPU/ChangeSpeed.cs
+++ Assets/WavesOnGPU/ChangeSpeed.cs
@@ -8,30 +8,49 @@ public class ChangeSpeed : MonoBehaviour
     private float step = 10f;
     private float radiusChange = 1f;
 
+    void Start()
+    {
+        StartCoroutine(delayUpdate());
+    }
+
+    private bool canUpdate = false; // Decides weather a function can update in update()
+
+    IEnumerator delayUpdate()
+    {
+        // Wait for 3 seconds
+        yield return new WaitForSeconds(3f);
+
+        // Allow updates to happen
+        canUpdate = true;
+    }
+
     void Update()
     {
-        //Press M to increase speed
-        if (Input.GetKeyDown(KeyCode.M))
+        if (canUpdate)
         {
-            changeForce.targetCircularSpeed += step;
-        }// Press L to decrease speed
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            changeForce.targetCircularSpeed -= step;
-        }
-        if (changeForce.behaviorID == 0)
-        {
-            //Press S for smaller radius
-            if (Input.GetKeyDown(KeyCode.S))
+            UnityEngine.Debug.Log("Speed>!!!");
+            //Press M to increase speed
+            if (Input.GetKeyDown(KeyCode.M))
             {
-                changeForce.targetRadius -= radiusChange;
-            }// Press B for bigger radius
-            if (Input.GetKeyDown(KeyCode.B))
+                changeForce.targetCircularSpeed += step;
+            }// Press L to decrease speed
+            if (Input.GetKeyDown(KeyCode.L))
             {
-                changeForce.targetRadius += radiusChange;
+                changeForce.targetCircularSpeed -= step;
             }
+            if (changeForce.behaviorID == 0)
+            {
+                //Press S for smaller radius
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    changeForce.targetRadius -= radiusChange;
+                }// Press B for bigger radius
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    changeForce.targetRadius += radiusChange;
+                }
 
+            }
         }
-
     }
 }
